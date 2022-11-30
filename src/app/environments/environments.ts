494b7f52ecf2226959980fba89a8be3f7247ee70
Andrew Ho <amh7705@psu.edu>
2022-11-30 01:44:10 -0500

    Progress

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather?location=baltimore&format=json&u=f',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '776c8bb437mshe4f84c0d3558630p1b4259jsn8bea61ac69a1'
+}
